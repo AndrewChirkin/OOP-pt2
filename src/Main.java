b0c46f0f5f8ff8004ac02f8d0eee41b59ba22916
Andrew <chirkash11@gmail.com>
2022-11-07 23:32:05 +0300

    hw1ex2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Human Maxim = new Human("Максим", "Минск", 1988, "бренд-менеджера");
-        Human Anna = new Human("Аня", "Москва", 1993, "методиста образовательных программ");
-        Human Kate = new Human("Катя", "Калининград", 1992, "продакт-менеджера");
-        Human Artem = new Human("Артём", "Москва", 1995, "директора по развитию бизнеса");
-        Maxim.greeting();
-        Anna.greeting();
-        Kate.greeting();
-        Artem.greeting();
-        Human Vladimir = new Human("Владимир", "Казань", 21, "111");
+        Flower Rose = new Flower("Роза", "", "Голландия", 35.59, 0);
+        Flower Chrizo = new Flower("Хризантема", "", "", 15, 5);
+        Flower Pion = new Flower("Пион", "", "", 69.9, 1);
+        Flower Gipsophyla = new Flower("Гипсофила", "", "Турция", 19.5, 10);
 
-
+        Pion.flowerInfo();
+        getBoo
 
 
     }
 
-}
+
+    }
+
